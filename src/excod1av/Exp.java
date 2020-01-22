1b1085873ffe29ccd491a88bd4c4c340c9bcce86
jalvarezbarciela <ceque@DESKTOP-TMBRIR9>
2020-01-22 13:39:19 +0100

    C

---
+++ src/excod1av/Exp.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excod1av;
+
+/**
+ *
+ * @author ceque
+ */
+public class Exp {
+    public void C(){
+        System.out.println("Commit C");
+    }
+}
